0ae5569dc0a5a4745721ee6ad9e1a5479cad3309
Citlalli Murillo <citmusa@hotmail.com>
2019-07-04 05:25:03 +0000

    post listing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Listing from "../components/listing"
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to this new Gatsby site.</p>
-    <p>We will be customizing this shortly. At the moment this page is living here: <a href="https://citmusa-gatsby01.netlify.com" target="_blank">https://citmusa-gatsby01.netlify.com</a></p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <h2>Some links</h2>
-    <ul>
-      <li><Link to="/page-2/">Go to page 2</Link></li>
-      <li><Link to="/company/">Company Index</Link></li>
-      <li><Link to="/company/about">Company About page</Link></li>
-    </ul>
+    <Listing />
   </Layout>
 )
 
